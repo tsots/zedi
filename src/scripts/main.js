078f639db271280825dd70fa283e0ecbcb8c2495
tsots <mosalatsotetsi@gmail.com>
2025-11-26 01:21:40 +0200

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+ 	const menu_btn = document.querySelector('.hamburger');
+	const mobile_menu = document.querySelector('.mobile');
+
+	menu_btn.addEventListener('click', function () {
+		menu_btn.classList.toggle('isActive');
+		mobile_menu.classList.toggle('isActive');
+	});
+}
